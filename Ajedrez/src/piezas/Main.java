3092fb4c90740bc864d012612ea4666c41844cfe
ireneFM97 <irenemeneses97@gmail.com>
2023-02-23 10:59:36 +0100

    semi-metodos en la clase juego

---
+++ Ajedrez/src/piezas/Main.java
@@ -0,0 +1,17 @@
+package piezas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		/*int mover_fila;
+		int mover_columna;
+		int seleccionar_fila = 0;
+		int seleccionar_columna = 0;
+		antes del metodo validarPosicion, poner esto
+		*/
+
+	}
+
+}
