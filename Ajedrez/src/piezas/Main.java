86a50e1a1d088a68d2d403050c44d1b019c6a04e
DESKTOP-KQ3PV3J\Sebas <tomasiewic@gmail.com>
2023-02-24 12:04:57 +0100

    creacion de metodos de seleccionar y mover pieza

--- Ajedrez/src/piezas/Main.java
+++ Ajedrez/src/piezas/Main.java
@@ -5,12 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		/*int mover_fila;
-		int mover_columna;
-		int seleccionar_fila = 0;
-		int seleccionar_columna = 0;
-		antes del metodo validarPosicion, poner esto
-		*/
+	
 
 	}
 
