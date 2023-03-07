aad6ad904e95345da608cc10cab1fd70aabfc368
ireneFM97 <irenemeneses97@gmail.com>
2023-03-07 18:35:22 +0100

    movimiento del peon con formato optimo

--- Ajedrez/src/piezas/Peones.java
+++ Ajedrez/src/piezas/Peones.java
@@ -36,126 +36,130 @@ public class Peones extends King{
 	public boolean mover(int mover_fila, int mover_columna, int seleccionar_columna, int seleccionar_fila, Piezas[][] tablero) {
 
 		if (tablero[mover_fila][mover_columna].isBlanco() != blanco) {
-			
-			//si es ficha blanca
+
+			// si es ficha blanca
 			if (blanco) {
-				//si está en la linea 2 para comenzar		
-				if (seleccionar_fila== 2) {
-					
+				// si está en la linea 2 para comenzar
+				if (seleccionar_fila == 2) {
+
 					// Se mueve solo una o dos
 					int dif_fila = mover_fila - seleccionar_fila;
-				    if (dif_fila == 1 || dif_fila == 2) {
-						
-				    	//fila tiene que ser una posicion de más
-				    	fija = seleccionar_columna;	
-						for (int i = seleccionar_fila ; i <= mover_fila ; i++) {
-							//ver si hay piezas por el camino
+					if (dif_fila == 1 || dif_fila == 2) {
+
+						// fila tiene que ser una posicion de más
+						fija = seleccionar_columna;
+						for (int i = seleccionar_fila; i <= mover_fila; i++) {
+							// ver si hay piezas por el camino
 							if (!tablero[i][fija].equals(Vacio)) {
-					            bien = false;
-							}	
+								bien = false;
+							}
 						}
 						System.out.print("Hay piezas por el camino, ");
-				    } else {
-				    	System.out.println("no entra porque no son los parametros");
-				    	bien = false;
-				    }			
-					
+					} else {
+						System.out.println("no entra porque no son los parametros");
+						bien = false;
+					}
+
 				} else {
-					//si la ficha no está para iniciar si no en mitad
+					// si la ficha no está para iniciar si no en mitad
 					if (seleccionar_fila > 2) {
-						
+
 						// Se mueve solo una posicion
 						int dif_fila = mover_fila - seleccionar_fila;
-					    
-						//verificar si exactamente solo se puede en una fila
+
+						// verificar si exactamente solo se puede en una fila
 						if (dif_fila == 1) {
-							
-							//fila tiene que ser una posicion de más
-					    	fija = seleccionar_columna;	
-							for (int i = seleccionar_fila ; i <= mover_fila ; i++) {
-								
+
+							// fila tiene que ser una posicion de más
+							fija = seleccionar_columna;
+							for (int i = seleccionar_fila; i <= mover_fila; i++) {
+
 								if (!tablero[i][fija].equals(Vacio)) {
-						            bien = false;
-								}	
+									bien = false;
+								}
 							}
 							System.out.print("Hay piezas por el camino, ");
 						} else {
-					    	System.out.println("no entra porque no son los parametros");
-					    	bien = false;
-					    }		
-	
-						//movemos en diagonal izquierda y diagonal derecha si hay una ficha del oponente
-						if ((tablero[seleccionar_fila + 1][seleccionar_columna - 1] != Vacio) || (tablero[seleccionar_fila + 1][seleccionar_columna + 1] != Vacio)) {
-							
+							System.out.println("no entra porque no son los parametros");
+							bien = false;
+						}
+
+						// movemos en diagonal izquierda y diagonal derecha si hay una ficha del
+						// oponente
+						if ((tablero[seleccionar_fila + 1][seleccionar_columna - 1] != Vacio)
+								|| (tablero[seleccionar_fila + 1][seleccionar_columna + 1] != Vacio)) {
+
 						} else {
 							System.out.println("no hay ninguna pieza para comer");
 							bien = false;
-						}  
+						}
 					} else {
 						System.out.println("estas selecionando una fila que no es, tienes que ir para arriba");
 						bien = false;
 					}
 				}
-				
+
 			} else {
-				//si está en la linea 8 para comenzar		
-				if (seleccionar_fila== 8) {
-					
+				// si está en la linea 8 para comenzar
+				if (seleccionar_fila == 8) {
+
 					// Se mueve solo una o dos
 					int dif_fila = mover_fila - seleccionar_fila;
-				    if (dif_fila == 1 || dif_fila == 2) {
-						
-				    	//fila tiene que ser una posicion de más
-				    	fija = seleccionar_columna;	
-						for (int i = seleccionar_fila ; i >= mover_fila ; i--) {
-							//ver si hay piezas por el camino
+					if (dif_fila == 1 || dif_fila == 2) {
+
+						// fila tiene que ser una posicion de más
+						fija = seleccionar_columna;
+						for (int i = seleccionar_fila; i >= mover_fila; i--) {
+							// ver si hay piezas por el camino
 							if (!tablero[i][fija].equals(Vacio)) {
-					            bien = false;
-							}	
+								bien = false;
+							}
 						}
 						System.out.print("Hay piezas por el camino, ");
-				    } else {
-				    	System.out.println("no entra porque no son los parametros");
-				    	bien = false;
-				    }			
-					
+					} else {
+						System.out.println("no entra porque no son los parametros");
+						bien = false;
+					}
+
 				} else {
-					//si la ficha no está para iniciar si no en mitad
+					// si la ficha no está para iniciar si no en mitad
 					if (seleccionar_fila < 8) {
-						
+
 						// Se mueve solo una posicion
 						int dif_fila = mover_fila - seleccionar_fila;
-					    
-						//verificar si exactamente solo se puede en una fila
+
+						// verificar si exactamente solo se puede en una fila
 						if (dif_fila == 1) {
-							
-							//fila tiene que ser una posicion de más
-					    	fija = seleccionar_columna;	
-							for (int i = seleccionar_fila ; i >= mover_fila ; i--) {
-								
+
+							// fila tiene que ser una posicion de más
+							fija = seleccionar_columna;
+							for (int i = seleccionar_fila; i >= mover_fila; i--) {
+
 								if (!tablero[i][fija].equals(Vacio)) {
-						            bien = false;
-								}	
+									bien = false;
+								}
 							}
 							System.out.print("Hay piezas por el camino, ");
 						} else {
-					    	System.out.println("no entra porque no son los parametros");
-					    	bien = false;
-					    }		
-	
-						//movemos en diagonal izquierda y diagonal derecha si hay una ficha del oponente
-						if ((tablero[seleccionar_fila - 1][seleccionar_columna - 1] != Vacio) || (tablero[seleccionar_fila - 1][seleccionar_columna + 1] != Vacio)) {
-							
+							System.out.println("no entra porque no son los parametros");
+							bien = false;
+						}
+
+						// movemos en diagonal izquierda y diagonal derecha si hay una ficha del
+						// oponente
+						if ((tablero[seleccionar_fila - 1][seleccionar_columna - 1] != Vacio)
+								|| (tablero[seleccionar_fila - 1][seleccionar_columna + 1] != Vacio)) {
+
 						} else {
 							System.out.println("no hay ninguna pieza para comer");
 							bien = false;
-						}  
+						}
 					} else {
 						System.out.println("estas selecionando una fila que no es, tienes que ir para arriba");
 						bien = false;
 					}
-			}
-			
+				}
+
 			}
 		}
 		return bien;
